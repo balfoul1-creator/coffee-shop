94057b257460808ef1e7cbd60271cf3568c1d433
samad <samadbalf@gmail.com>
2026-03-01 00:56:10 +0000

    Testing with intentinal bug priced

--- coffee-shop.js
+++ coffee-shop.js
@@ -7,7 +7,7 @@ function calculateBill(coffeeType, quantity) {
   if (coffeeType === "espresso") {
     price = 3;
   } else if (coffeeType === "latte") {
-    price = 4;
+    price = 5;  // BUG!
   } else if (coffeeType === "cappuccino") {
     price = 4;
   } else if (coffeeType === "mocha") {
